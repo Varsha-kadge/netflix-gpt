ddaf5c40d4f340d69047c1144e249b104bcd6529
Varsha kadge <varshakadgem4@gmail.com>
2025-07-09 11:37:11 +0530

    Added hover effect on movie cards

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -2,7 +2,7 @@ import { IMG_CDN } from '../Utils/Constant';
 const MovieCard = ({ posterPath }) => {
   if (!posterPath) return null;
   return (
-    <div className='w-48 pr-4'>
+    <div className='w-48 cursor-pointer pr-4 transition-transform duration-300 hover:scale-105 hover:shadow-xl'>
       <img alt='Movie Card' src={IMG_CDN + posterPath} />
     </div>
   );
